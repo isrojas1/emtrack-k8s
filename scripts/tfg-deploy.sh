c9f6beaeca00e641c415ec8a92db073760fcfec6
isrojas1 <ivansanchez@uma.es>
2025-03-16 10:55:26 +0100

    Improvements to tfg-deploy.sh and remove harbor-deploy.sh

--- scripts/tfg-deploy.sh
+++ scripts/tfg-deploy.sh
@@ -3,19 +3,32 @@
 # WARNING: This scripts asumes you already have harbor installed and set in
 #           /etc/rancher/k3s/registries.yaml config file in your node(s)
 
+# Set env variables
+if [ $# -ne 1 ]; then
+  echo "Usage: ./scripts/tfg-deploy.sh <ENV_FILE>"
+  exit 1
+fi
+
+ENV_FILE=$1
+source "$ENV_FILE" ||{ echo "Error sourcing env file"; exit 1; }
+
 # Create namespaces
 kubectl apply -f ./deployments/namespaces.yaml
 
 # Add helm charts
 helm repo add ertis https://ertis-research.github.io/Helm-charts/
+helm repo add harbor https://helm.goharbor.io
 
 # Install helm charts
-helm upgrade --install opentwins ertis/OpenTwins --wait --dependency-update --debug \
+helm upgrade --install opentwins ertis/OpenTwins --wait --dependency-update \
              -f "./helms/opentwins/values.yaml" \
              --namespace opentwins
 
+# Deploy harbor
+envsubst < ./helms/harbor/values.yaml | helm upgrade --install harbor harbor/harbor -f - -n harbor
+
 # Create necessary connections
-bash ./dittoapi/connections.sh
+# bash ./dittoapi/connections.sh
 
 # Deploy components
-kubectl apply -f ./deployments/emtscraper.yaml
+envsubst < ./deployments/emtscraper/buslocation.yaml | kubectl apply -f -
