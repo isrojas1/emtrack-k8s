85ebae5f6d3e346bdd68393ba674dd5bb600415e
isrojas1 <ivansanchez@uma.es>
2025-03-16 10:56:21 +0100

    Proper harbor-deploy.sh deletion

--- scripts/harbor-deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-kubectl create namespace harbor
-
-helm repo add harbor https://helm.goharbor.io
-
-# WARNING: this way has not been tested, refer to https://goharbor.io/docs/2.12.0/install-config/harbor-ha-helm/
-#           if problems arise
-helm install harbor harbor/harbor -f ./helms/harbor/values.yaml -n harbor
