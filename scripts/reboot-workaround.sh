5ed03f2211de02d7ddf2e051009da4b34bc1991e
isrojas1 <ivansanchez@uma.es>
2025-03-16 10:52:20 +0100

    Add emtscraper-buslocation restart to workaround

--- scripts/reboot-workaround.sh
+++ scripts/reboot-workaround.sh
@@ -2,3 +2,5 @@
 
 kubectl rollout restart deployment/opentwins-mosquitto -n opentwins
 kubectl rollout restart deployment/opentwins-telegraf -n opentwins
+
+kubectl rollout restart deployment/emtscraper-buslocation -n scraping
