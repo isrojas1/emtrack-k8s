97598232558563115d75007ca3369392fca96518
isrojas1 <ivansanchez@uma.es>
2025-06-17 18:42:48 +0200

    Add mysql components

--- scripts/tfg-deploy.sh
+++ scripts/tfg-deploy.sh
@@ -23,9 +23,24 @@ helm upgrade --install opentwins ertis/OpenTwins --wait --dependency-update \
              -f "./helms/opentwins/values.yaml" \
              --namespace opentwins
 
+# Use my local fork
+# helm upgrade --install opentwins ../../OpenTwins --wait --dependency-update \
+#              -f "./helms/opentwins/values.yaml" \
+#              --namespace opentwins
+
 # Push harbor images
 docker push --all-tags "${NODE_IP}:30002/library/buslocation"
+docker push --all-tags "${NODE_IP}:30002/library/routes"
+
+# Deploy mysql
+kubectl create namespace mysql
+envsubst < ./secrets/mysql-secret.yaml | kubectl apply -f -
+kubectl apply -f ./services/mysql.yaml
+kubectl apply -f ./statefulsets/mysql.yaml
+kubectl create secret generic mysql-secret \
+  --from-literal=loader_password="$MYSQL_PASSWORD" \
 
 # Deploy components
 envsubst < ./deployments/emtscraper/buslocation.yaml | kubectl apply -f -
 envsubst < ./cronjobs/emtscraper/routes.yaml | kubectl apply -f -
+kubectl apply -f cronjobs/emtscraper/mysql-csv-loader.yaml
